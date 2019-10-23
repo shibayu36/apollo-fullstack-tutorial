e352d1841998daa61774bd10799f315e04014d2e
shibayu36 <shibayu36@gmail.com>
2019-10-23 21:54:17 +0900

    resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,7 +2,16 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
 type Query {
-  launches: [Launch]!
+  launches( # replace the current launches query with this one.
+    """
+    The number of results to show. Must be >= 1. Default = 20
+    """
+    pageSize: Int
+    """
+    If you add a cursor here, it will only return results _after_ this cursor
+    """
+    after: String
+  ): LaunchConnection!
   launch(id: ID!): Launch
   # Queries for the current user
   me: User
@@ -16,6 +25,17 @@ type Mutation {
   cancelTrip(launchId: ID!): TripUpdateResponse!
 
   login(email: String): String # login token
+}
+
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
 }
 
 type Launch {

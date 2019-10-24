1d98243d20eef138716e04245e1b68d26721b228
shibayu36 <shibayu36@gmail.com>
2019-10-24 22:20:26 +0900

    apollo.config.js

---
+++ start/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: "apollo-fullstack-tutorial",
+      url: "http://localhost:4000/graphql",
+      includes: ['./client/src/**/*.js']
+    }
+  }
+};
